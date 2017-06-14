0bf4a72414679bb11ddf94729921a65fb3c16613
Phil Banks <phil@helpfultechnology.com>
2017-06-14 11:16:55 +0100

    First crack
    Allow all permitted media types;
    Leverage `wp media import` command instead of self-styled.

--- command.php
+++ command.php
@@ -39,25 +39,28 @@ $run_sideload_media_command = function( $args, $assoc_args ) {
 
 	$where_parts = array();
 
-	$domain_str = '%' . esc_url_raw( $assoc_args['domain'] ) . '%';
-	$where_parts[] = $wpdb->prepare( "post_content LIKE %s", $domain_str );
-
+	// Build database query.
+	$domain_str = '%' . esc_url_raw( rtrim( $assoc_args['domain'], '/' ) ) . '%';
+	$where_parts[] = $wpdb->prepare( 'post_content LIKE %s', $domain_str );
 	if ( ! empty( $assoc_args['post_type'] ) ) {
-		$where_parts[] = $wpdb->prepare( "post_type = %s", sanitize_key( $assoc_args['post_type'] ) );
+		$where_parts[] = $wpdb->prepare( 'post_type = %s', sanitize_key( $assoc_args['post_type'] ) );
 	} else {
 		$where_parts[] = "post_type NOT IN ('revision')";
 	}
-
 	if ( ! empty( $where_parts ) ) {
 		$where = 'WHERE ' . implode( ' AND ', $where_parts );
 	} else {
 		$where = '';
 	}
-
 	$query = "SELECT ID, post_content FROM $wpdb->posts $where";
 
+	// Prepare domain for use in regex.
+	$domain_str_regex = preg_quote( $domain_str );
+
 	$num_updated_posts = 0;
-	foreach( new WP_CLI\Iterators\Query( $query ) as $post ) {
+	$all_srcs = array();
+	// Loop over all posts returned by database query.
+	foreach ( new WP_CLI\Iterators\Query( $query ) as $post ) {
 
 		$num_sideloaded_images = 0;
 
@@ -65,57 +68,44 @@ $run_sideload_media_command = function( $args, $assoc_args ) {
 			continue;
 		}
 
-		$document = new DOMDocument;
-		@$document->loadHTML( $post->post_content );
+		// Only import filetypes permitted by WP settings.
+		$allowed_mimes = implode( '|', array_keys( get_allowed_mime_types() ) );
 
-		$img_srcs = array();
-		foreach( $document->getElementsByTagName( 'img' ) as $img ) {
+		// Get media items from the post content.
+		preg_match_all( "/($domain_str_regex\/.*\.($allowed_mimes))(?:\s|\"+)/i", $post->post_content, $matches );
 
-			// Sometimes old content management systems put spaces in the URLs
-			$img_src = esc_url_raw( str_replace( ' ', '%20', $img->getAttribute( 'src' ) ) );
-			if ( ! empty( $assoc_args['domain'] ) && $assoc_args['domain'] != parse_url( $img_src, PHP_URL_HOST ) ) {
+		$this_post_srcs = array();
+		// Loop over all media items found in the post.
+		foreach ( $matches[1] as $match ) {
+
+			// Sometimes old content management systems put spaces in the URLs.
+			$item_src = esc_url_raw( str_replace( ' ', '%20', $match ) );
+			if ( ! empty( $assoc_args['domain'] ) && $assoc_args['domain'] != parse_url( $item_src, PHP_URL_HOST ) ) {
 				continue;
 			}
 
-			// Don't permit the same media to be sideloaded twice for this post
-			if ( in_array( $img_src, $img_srcs ) ) {
+			// Don't permit the same media to be sideloaded twice for this post.
+			if ( in_array( $item_src, $this_post_srcs ) ) {
 				continue;
 			}
 
-			// Most of this was stolen from media_sideload_image
-			$tmp = download_url( $img_src );
+			// Import the media file.
+			// BUG: Needs to check if file already exists and get existing file ID if so.
+			$new_item_id = WP_CLI::runcommand( "media import {$item_src} --post_id={$post->ID} --porcelain", array( 'launch' => false ) );
 
-			// Set variables for storage
-			// fix file filename for query strings
-			preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $img_src, $matches );
-			$file_array = array();
-			$file_array['name'] = sanitize_file_name( urldecode( basename( $matches[0] ) ) );
-			$file_array['tmp_name'] = $tmp;
+			// Get newly imported item URL.
+			$new_item_url = wp_get_attachment_url( $id );
+			// Replace old item URL with new item URL.
+			$post->post_content = str_replace( $match, $new_item_url, $post->post_content );
 
-			// If error storing temporarily, unlink
-			if ( is_wp_error( $tmp ) ) {
-				@unlink( $file_array['tmp_name'] );
-				$file_array['tmp_name'] = '';
-				WP_CLI::warning( $tmp->get_error_message() );
-				continue;
-			}
-
-			// do the validation and storage stuff
-			$id = media_handle_sideload( $file_array, $post->ID );
-			// If error storing permanently, unlink
-			if ( is_wp_error( $id ) ) {
-				@unlink( $file_array['tmp_name'] );
-				WP_CLI::warning( $id->get_error_message() );
-				continue;
-			}
-
-			$new_img = wp_get_attachment_image_src( $id, 'full' );
-			$post->post_content = str_replace( $img->getAttribute( 'src' ), $new_img[0], $post->post_content );
+			// Update records.
 			$num_sideloaded_images++;
-			$img_srcs[] = $img_src;
+			$this_post_srcs[] = $img_src;
+			$all_srcs[] = $img_src;
 
+			// Inform user about progress.
 			if ( $assoc_args['verbose'] ) {
-				WP_CLI::line( sprintf( "Replaced '%s' with '%s' for post #%d", $img_src, $new_img[0], $post->ID ) );
+				WP_CLI::line( sprintf( "Replaced '%s' with '%s' for post #%d", $match, $new_img_url, $post->ID ) );
 			}
 
 		}
@@ -125,14 +115,14 @@ $run_sideload_media_command = function( $args, $assoc_args ) {
 			$wpdb->update( $wpdb->posts, array( 'post_content' => $post->post_content ), array( 'ID' => $post->ID ) );
 			clean_post_cache( $post->ID );
 			if ( $assoc_args['verbose'] ) {
-				WP_CLI::line( sprintf( "Sideloaded %d media references for post #%d", $num_sideloaded_images, $post->ID ) );
+				WP_CLI::line( sprintf( 'Sideloaded %d media references for post #%d', $num_sideloaded_images, $post->ID ) );
 			}
-		} else if ( ! $num_sideloaded_images && $assoc_args['verbose'] ) {
-			WP_CLI::line( sprintf( "No media sideloading necessary for post #%d", $post->ID ) );
+		} elseif ( ! $num_sideloaded_images && $assoc_args['verbose'] ) {
+			WP_CLI::line( sprintf( 'No media sideloading necessary for post #%d', $post->ID ) );
 		}
 	}
 
-	WP_CLI::success( sprintf( "Sideload complete. Updated media references for %d posts.", $num_updated_posts ) );
+	WP_CLI::success( sprintf( 'Sideload complete. Updated media references for %d posts.', $num_updated_posts ) );
 };
 
 WP_CLI::add_command( 'media sideload', $run_sideload_media_command );
